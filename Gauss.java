4844c9cfff8caaa1f82ebbc5cec70e600c605c2c
dimasssek <90604920+dimasssek@users.noreply.github.com>
2022-10-04 23:54:28 +0300

    Delete Gauss.java

--- Gauss.java
+++
@@ -1,9 +0,0 @@
-package ru.chislaki;
-
-public interface Gauss<N extends Number, T extends Gauss<N, T>> {
-    void addEquation(T item);
-    void mul(N coefficient);
-    N findCoefficient(N a, N b);
-    N at(int index);
-    int size();
-}
